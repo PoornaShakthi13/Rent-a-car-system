30596339e0a69709cb1af38c4e10b03662b63a3d
PoornaShakthi13 <shakthihw13@gmail.com>
2023-02-18 22:43:55 +0530

    back-end packages created

---
+++ back-end/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,19 @@
+package lk.ijse.spring.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({JPAConfig.class})
+@ComponentScan(basePackageClasses = {})
+public class WebRootConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
